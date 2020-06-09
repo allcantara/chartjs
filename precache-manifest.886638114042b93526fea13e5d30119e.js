97bfe2119b8dc26ee690eb85db170d441d942007
Bruno Alcântara <brunoalcantarajc@gmail.com>
2020-06-09 12:09:16 -0300

    Updates

---
+++ precache-manifest.886638114042b93526fea13e5d30119e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eefbc66408ad02744fa0e1bf48143f4b",
+    "url": "/chartjs/index.html"
+  },
+  {
+    "revision": "795deaa5db3b8353063f",
+    "url": "/chartjs/static/css/main.93eb6935.chunk.css"
+  },
+  {
+    "revision": "26ece5eec5ffcc95ed18",
+    "url": "/chartjs/static/js/2.09c80c52.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/chartjs/static/js/2.09c80c52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "795deaa5db3b8353063f",
+    "url": "/chartjs/static/js/main.ceeac05b.chunk.js"
+  },
+  {
+    "revision": "190afe5a7bcf491cb594",
+    "url": "/chartjs/static/js/runtime-main.106a26f8.js"
+  }
+]);
